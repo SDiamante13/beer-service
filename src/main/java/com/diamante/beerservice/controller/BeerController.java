86db7d810fe4fa07fcdb7d8b9200ba2a42dbce54
Steven Diamante <steven@pivotal.io>
2019-08-06 19:59:36 -0500

    Add Beer Controller stubs for GET, POST, and PUT. [Closes #3]

---
+++ src/main/java/com/diamante/beerservice/controller/BeerController.java
@@ -0,0 +1,28 @@
+package com.diamante.beerservice.controller;
+
+import com.diamante.beerservice.model.Beer;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+@RequestMapping("/api/v1/beer")
+@RestController
+public class BeerController {
+
+    @GetMapping("{/beerId}")
+    public ResponseEntity<Beer> getBeerById(@PathVariable("/beerId") UUID beerId) {
+        return new ResponseEntity<>(Beer.builder().build(), HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity saveNewBeer(@RequestBody Beer beer) {
+        return new ResponseEntity(HttpStatus.CREATED);
+    }
+
+    @PutMapping("{/beerId}")
+    public ResponseEntity updateBeerById(@PathVariable("/beerId") UUID beerId, @RequestBody Beer beer) {
+        return new ResponseEntity(HttpStatus.NO_CONTENT);
+    }
+}
