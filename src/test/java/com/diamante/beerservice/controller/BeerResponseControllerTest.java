2f5b33675662a16dcba7f74063bcbaa1325e755e
Steven Diamante <steven@pivotal.io>
2019-09-19 22:05:10 -0500

    Add Validation Error Handling. [Closes #9]

--- src/test/java/com/diamante/beerservice/controller/BeerResponseControllerTest.java
+++ src/test/java/com/diamante/beerservice/controller/BeerResponseControllerTest.java
@@ -1,6 +1,7 @@
 package com.diamante.beerservice.controller;
 
 import com.diamante.beerservice.model.BeerDto;
+import com.diamante.beerservice.model.BeerStyleEnum;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -10,6 +11,8 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.math.BigDecimal;
+import java.util.Random;
 import java.util.UUID;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
@@ -23,7 +26,14 @@ class BeerControllerTest {
     private MockMvc mockMvc;
 
     @Autowired
-    ObjectMapper objectMapper;
+    private ObjectMapper objectMapper;
+
+    private BeerDto.BeerDtoBuilder beerDtoBuilder = BeerDto
+            .builder()
+            .beerName("Golden Monkey")
+            .beerStyle(BeerStyleEnum.LAGER)
+            .upc(3672676L)
+            .price(BigDecimal.valueOf(12.99));
 
     @Test
     void getBeerById() throws Exception {
@@ -34,8 +44,7 @@ class BeerControllerTest {
 
     @Test
     void saveNewBeer() throws Exception {
-        BeerDto beerDto = BeerDto.builder().build();
-        String beerJson = objectMapper.writeValueAsString(beerDto);
+        String beerJson = objectMapper.writeValueAsString(beerDtoBuilder.build());
 
         mockMvc.perform(post("/api/v1/beer")
                 .contentType(MediaType.APPLICATION_JSON)
@@ -45,7 +54,7 @@ class BeerControllerTest {
 
     @Test
     void updateBeerById() throws Exception {
-        BeerDto beerDto = BeerDto.builder().build();
+        BeerDto beerDto = beerDtoBuilder.quantityOnHand(2).build();
         String beerJson = objectMapper.writeValueAsString(beerDto);
 
         mockMvc.perform(put("/api/v1/beer/" + UUID.randomUUID().toString())
